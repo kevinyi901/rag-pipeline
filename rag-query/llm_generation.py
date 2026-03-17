50033daad06c40b26ea8bdbe83dd0b034f197121
Evalynna Ong <evalynna.ong@gmail.com>
2026-03-17 16:58:01 -0700

    Add line for prompt caching

--- rag-query/llm_generation.py
+++ rag-query/llm_generation.py
@@ -94,6 +94,7 @@ def generate_llm_response(query_text: str, context_string: str) -> str:
     message = client.messages.create(
         model=Config.CLAUDE_MODEL,
         max_tokens=Config.MAX_NEW_TOKENS,
+        cache_control=[{"type": "ephemeral"}],      # invoke automatic caching
         system=system_prompt,
         messages=[{"role": "user", "content": user_prompt}],
     )
@@ -137,6 +138,7 @@ def generate_llm_response_filter_only_search(
     message = client.messages.create(
         model=Config.CLAUDE_MODEL,
         max_tokens=Config.MAX_NEW_TOKENS,
+        cache_control=[{"type": "ephemeral"}],  # invoke automatic caching
         system=system_prompt,
         messages=[{"role": "user", "content": user_prompt}],
     )
